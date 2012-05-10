b8c1f8096c507f1e032f47b477245d90b25df18c
Ward Poelmans <ward.poelmans@ugent.be>
2012-05-10 11:28:55 +0200

    Added readin methods for HDF5 files
    The method ReadInitfromFile read the setupdata from the HDF5 file and
    then after calling all init's, you can readin the actual TPM with
    ReadfromFile.

--- include/TPM.h
+++ include/TPM.h
@@ -96,6 +96,10 @@ class TPM : public BlockMatrix {
 
       int SaveToFile(const char *);
 
+      static int ReadInitfromFile(const char *filename, std::string &setupdata);
+
+      int ReadfromFile(const char *);
+
    private:
 
       //!list that relates the blockindex to physical two-particle quantumnumbers
