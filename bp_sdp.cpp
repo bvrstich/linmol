0822c4f43d51bc73757712daaac65d63be41060a
Brecht Verstichel <brecht.verstichel@ugent.be>
2012-05-22 14:05:28 +0200

    definitely something wrong here
    wrong number of particles in the subspace

--- bp_sdp.cpp
+++ bp_sdp.cpp
@@ -71,153 +71,30 @@ int main(void){
    TPM ham;
    ham.molecule(si);
 
-   TPM ham_copy(ham);
+   ifstream in("/home/bright/bestanden/results/linmol/BeB/DM_out/BeB-3.rdm");
 
-   TPM S_2;
-   S_2.set_S_2();
+   TPM rdm;
 
-   //only traceless hamiltonian needed in program.
-   ham.proj_Tr();
+   for(int B = 0;B < rdm.gnr();++B)
+      for(int i = 0;i < rdm.gdim(B);++i)
+         for(int j = i;j < rdm.gdim(B);++j)
+            in >> B >> i >> j >> rdm(B,i,j);
 
-   //primal
-   SUP X;
+   rdm.symmetrize();
 
-   //dual
-   SUP Z;
+   cout << N*(N - 1)/2 << "\t" << rdm.trace() << "\t" << rdm.ddot(ham) + CartInt::gNucRepEn() << endl;
 
-   //Lagrange multiplier
-   SUP V;
+   //make the subsystem objects
+   SubSys ss_Be(0,si);
+   ss_Be.setBe();
 
-   //just dubya
-   SUP W;
+   cout << rdm.ddot(ss_Be.gsubocc()) << "\t" << rdm.ddot(ss_Be.subham()) << endl;
 
-   SUP u_0;
+   SubSys ss_B(1,si);
+   ss_B.setB();
 
-   //little help
-   TPM hulp;
-
-   u_0.gI().unit();
-
-   u_0.fill();
-
-   X = 0.0;
-   Z = 0.0;
-
-   //what does this do?
-   double sigma = 1.0;
-
-   double tolerance = 1.0e-7;
-
-   double D_conv(1.0),P_conv(1.0),convergence(1.0);
-
-   // mazziotti uses 1.6 for this
-   double mazzy = 1.9;
-
-   int iter_dual,iter_primal(0);
-   int max_iter = 1;
-
-   int tot_iter = 0;
-
-   while(P_conv > tolerance || D_conv > tolerance || fabs(convergence) > tolerance){
-
-      ++iter_primal;
-
-      D_conv = 1.0;
-
-      iter_dual = 0;
-
-      while(D_conv > tolerance  && iter_dual <= max_iter)
-      {
-         ++tot_iter;
-
-         ++iter_dual;
-
-         //solve system
-         SUP B(Z);
-
-         B -= u_0;
-
-         B.daxpy(mazzy/sigma,X);
-
-         TPM b;
-
-         b.collaps(1,B);
-
-         b.daxpy(-mazzy/sigma,ham);
-
-         hulp.S_L(-1,b);
-
-         //hulp is the matrix containing the gamma_i's
-         hulp.proj_Tr();
-
-         //construct W
-         W.fill(hulp);
-
-         W += u_0;
-
-         W.daxpy(-1.0/sigma,X);
-
-         //update Z and V with eigenvalue decomposition:
-         W.sep_pm(Z,V);
-
-         V.dscal(-sigma);
-
-         //check infeasibility of the primal problem:
-         TPM v;
-
-         v.collaps(1,V);
-
-         v -= ham;
-
-         D_conv = sqrt(v.ddot(v));
-
-      }
-
-      //update primal:
-      X = V;
-
-      //check dual feasibility (W is a helping variable now)
-      W.fill(hulp);
-
-      W += u_0;
-
-      W -= Z;
-
-      P_conv = sqrt(W.ddot(W));
-
-      if(D_conv < P_conv)
-         sigma *= 1.01;
-      else
-         sigma /= 1.01;
-
-      convergence = ham.ddot(Z.gI()) + u_0.ddot(X);
-
-      cout << P_conv << "\t" << D_conv << "\t" << sigma << "\t" << convergence << "\t" << ham_copy.ddot(Z.gI()) + CartInt::gNucRepEn() << "\t" << Z.gI().ddot(S_2) << endl;
-
-   }
-
-   cout << endl;
-   cout << "Energy: " << ham_copy.ddot(Z.gI()) + CartInt::gNucRepEn() << endl;
-   cout << "Spin: " << S_2.ddot(Z.gI())<< endl;
-   cout << "pd gap: " << Z.ddot(X) << endl;
-   cout << "dual conv: " << D_conv << endl;
-   cout << "primal conv: " << P_conv << endl;
-
-   cout << endl;
-   cout << "total nr of iterations = " << tot_iter << endl;
-
-   LinIneq::clear();
-
-   PPHM::clear();
-   DPM::clear();
-   PHM::clear();
-   TPM::clear();
-   SPM::clear();
-
-   Tools::clear();
-
-   SphInt::clear();
-   CartInt::clear();
+   cout << rdm.ddot(ss_B.gsubocc()) << "\t" << rdm.ddot(ss_B.subham()) << endl;
 
    return 0;
+
 }
